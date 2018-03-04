c2576135a112d9e65d8fc1e04641b6e4616f6539
Andrew <Andrew@testdembp.home>
2018-03-04 18:28:03 +0000

    Added game logic

--- js/main.js
+++ js/main.js
@@ -1,7 +1,21 @@
-var cardOne = "king"
-var cardTwo = "king"
-var cardThree = "queen"
-var cardFour = "queen"
+// console.log("user flipped " + cardOne)；
+// console.log("user flipped " + cardThree)；
 
-console.log("user flipped " + cardOne)
-console.log("user flipped " + cardThree)
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+var cardTwo = cards[2];
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+console.log("User flipped " + cardsInPlay[0]);
+console.log("User flipped " + cardsInPlay[1]);
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!")
+	} else {
+		alert("Sorry, try again.")
+	}
+}
